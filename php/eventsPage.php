ba54ae13b308ab0181fc09ec7680231189cc7e91
Matteo Innocenti <matteo.innocenti2@studio.unibo.it>
2019-11-28 19:50:56 +0100

    Events page created and new logos and wallpapers added

---
+++ php/eventsPage.php
@@ -0,0 +1,35 @@
+<div class="row">
+    <div class="col-md-12 col-sm-12 mt-5">
+        <h2 class="text-light display-4">Eventi in programma</h2>
+        <div class="row">
+            <div class="form-div col-md-6 col-xs-12">
+                <label>Martin Garrix in Concert</label>
+                <input type="text" class="form-control" id="username">
+            </div>
+            <div class="form-div col-md-6 col-xs-12">
+                <label>Martin Garrix in Concert</label>
+                <input type="text" class="form-control" id="username">
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-div col-md-6 col-xs-12">
+                <label>Martin Garrix in Concert</label>
+                <input type="text" class="form-control" id="username">
+            </div>
+            <div class="form-div col-md-6 col-xs-12">
+                <label>Martin Garrix in Concert</label>
+                <input type="text" class="form-control" id="username">
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-div col-md-6 col-xs-12">
+                <label>Martin Garrix in Concert</label>
+                <input type="text" class="form-control" id="username">
+            </div>
+            <div class="form-div col-md-6 col-xs-12">
+                <label>Martin Garrix in Concert</label>
+                <input type="text" class="form-control" id="username">
+            </div>
+        </div>
+    </div>
+</div>
