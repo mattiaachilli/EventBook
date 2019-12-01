707680c673aec010abe7ce665dbbae2be1d3b16e
Christian D'Errico <christian.derrico@studio.unibo.it>
2019-12-01 18:13:03 +0100

    List item modified

---
+++ php/benvenutoLabel.php
@@ -0,0 +1,4 @@
+<?php 
+    require_once("bootstrap.php");
+?>
+<a class = "dropdown-item text-primary" href="login.php">Benvenuto <?php echo $_SESSION["user"]?></a>
---
+++ php/elements.php
@@ -0,0 +1,2 @@
+<a class = "dropdown-item text-primary" href="login.php">Accedi</a>
+<a class = "dropdown-item text-primary" href="registration.php">Registrati</a>
