c0d8e969aa80cef7b8501edac143d89bae4e389b
Christian D'Errico <christian.derrico@studio.unibo.it>
2019-12-03 23:20:39 +0100

    Js table modified

--- js/table.js
+++ js/table.js
@@ -1,5 +1,15 @@
 function insertRows(data){
     let result;
+    result = `<table class="table text-white border-bottom" id = "table">
+    <caption class = "text-white text-center font-italic mt-4 p-0">List of events</caption>
+    <thead>
+        <tr>
+            <th class = "border-top-0 text-center" id = "organizzatore" scope="col">Organizzatore</th>
+            <th class = "border-top-0 text-center" id = "evento" scope="col">Evento</th>
+            <th class = "border-0 text-center" id = "buttons" scope = "col"></th>
+        </tr>
+    </thead>
+    <tbody>`;
     for(let i = 0; i < data.length; i++){
         result += `<tr>
                 <td headers = "organizzatore" class = "text-center">${data[i]["Username"]}</td>
@@ -14,15 +24,16 @@ function insertRows(data){
                 </td>
             </tr>`;
     }
-    return result;
+    return result += `</tbody>
+    </table>`;
 }
 
 $(document).ready(function(){
     $("#appButton").click(function(){
-        $(".form-div").load("phpPages/tablePage.php");
+        //$(".form-div").load("phpPages/tablePage.php");
         $.getJSON("../api/api-table.php", function(data){
             let rows = insertRows(data);
-            $("tbody").html("").append(rows);
+            $(".form-div").html("").append(rows);
         });
     });
 
