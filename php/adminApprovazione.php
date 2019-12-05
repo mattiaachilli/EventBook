d9722f4497df25142c70a33e558efc611855a475
Christian D'Errico <christian.derrico@studio.unibo.it>
2019-12-05 22:37:13 +0100

    Approvation page completed

--- php/adminApprovazione.php
+++ php/adminApprovazione.php
@@ -3,8 +3,9 @@
 
     if(isUserLoggedIn() && typeOfUserLogged() == ADMIN) {
         $parameters["title"] = "Admin - EventBook";
+        $parameters["js"] = array("../js/table.js", "../js/insertRows.js");
+        $parameters["events"] = $db->getEvents();
         $parameters["content"] = "phpPages/tableApprovazioni.php";
-        $parameters["js"] = array("../js/table.js");
     } else {
         header("Location: index.php");
     }
--- php/phpPages/tableApprovazioni.php
+++ php/phpPages/tableApprovazioni.php
@@ -4,7 +4,6 @@
         <h2 class="text-light display-4 text-center mb-5">Lista eventi da approvare</h2>
         <div class = "form-div rounded">
             <table class="table text-white border-bottom" id = "table">
-                <caption class = "text-white text-center font-italic mt-4 p-0">List of events</caption>
                 <thead>
                     <tr>
                         <th class = "border-top-0 text-center" id = "organizzatore" scope="col">Organizzatore</th>
@@ -13,6 +12,23 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <?php foreach($parameters["events"] as $event): ?>
+                        <tr>
+                            <td headers = "organizzatore" class = "text-center"><?php echo $event["Username"]?></td>
+                            <td headers = "evento" class = "text-center"><?php echo $event["Nome_evento"]?></td>
+                            <td headers = "buttons" class = "text-center row m-0">
+                                <form class = "text-center row container-fluid mt-0" action = "../api/api-approvation.php" class = "button-form" type = "post">
+                                    <div class = "col-6">
+                                        <button type="submit" class="yButton btn btn-primary container-fluid">Y</button>
+                                    </div>
+                                    <div class = "col-6">
+                                        <button type="submit" class="fButton btn btn-primary container-fluid">N</button>
+                                    </div>
+                                    <input type = "hidden" value = "<?php echo $event["IDevento"]?>">
+                                </form>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
