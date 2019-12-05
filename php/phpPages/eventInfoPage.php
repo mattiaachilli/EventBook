b362d074e36ce54f1a0e6df0957cd05ec7c6d9d8
Matteo Innocenti <matteo.innocenti2@studio.unibo.it>
2019-12-05 15:29:32 +0100

    Info event page done

---
+++ php/phpPages/eventInfoPage.php
@@ -0,0 +1,58 @@
+<div class="row  mt-5">
+    <div class="col-sm-1 col-xs-0"></div>
+    <div class="col-sm-10 col-xs-12">
+        <h2 class="text-light display-4">ARMIN VAN BUUREN IN CONCERT</h2>
+    </div>
+    <div class="col-sm-1 col-xs-0"></div>
+</div>
+
+<div class="row text-light mt-2">
+    <div class="col-sm-1 col-xs-0"></div>
+    <div class="col-sm-10 col-xs-12">
+        <div class="form-div rounded">
+            <div class="row">
+                <div class="col-xl-4 col-xs-12 col-sm-12">
+                    <img src="../img/events/armin.jpg" width="75%"/>
+                </div>
+                <div class="col-xl-8 col-xs-12 col-sm-12">
+                    <div class="row">
+                        <div class="col-12">
+                            Armin van Buuren has won a number of accolades. 
+                            He has been ranked the number one DJ by DJ Mag a record of five times, four years in a row. 
+                            He was ranked fourth on the DJ Mag Top 100 DJs list in 2015, 2016, and 2019, and third in 2017. 
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-12 text-truncate">
+                            Indirizzo: Amsterdam Arena - ArenA Boulevard 1, 1100 DL Amsterdam, Paesi Bassi
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-12 font-weight-light font-italic text-truncate">Genere: musica elettronica</div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-xl-2 col-sm-12 col-xs-12">
+                            Prezzo: 50€
+                        </div>
+                        <div class="col-xl-3 col-sm-0 col-xs-0"></div>
+                        <div class="col-xl-3 col-sm-5 col-xs-5 w-50 mt-2">
+                            <select class="custom-select" id="location">
+                            <option value="">N. biglietti</option>
+                                <?php
+                                    for($i = 0; $i < 21; $i++) {
+                                        echo '<option>'.$i.'</option>';
+                                    }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="col-xl-4 col-sm-12 col-xs-12 mt-2">
+                            <button type="submit" class="btn btn-primary">Aggiungi al carrello</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-1 col-xs-0"></div> 
+</div>
+
