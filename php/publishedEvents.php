8cd85c91f7bbf0fe8c92af698d1d5747af30c3bf
Matteo Innocenti <matteo.innocenti2@studio.unibo.it>
2019-12-08 16:52:55 +0100

    Published events page created

---
+++ php/publishedEvents.php
@@ -0,0 +1,13 @@
+<?php
+    require_once(dirname(__DIR__)."/php/bootstrap.php");
+
+    if(isUserLoggedIn() && typeOfUserLogged() == ORGANIZER) {
+        $parameters["title"] = "Eventi pubblicati";
+        $parameters["content"] = dirname(__DIR__)."/php/phpPages/publishedEventsPage.php";
+        $parameters["events"] = $db->getPublishiedEvents($_SESSION["user"][0]);
+        //$parameters["js"] = array("../js/cart.js");
+    } else {
+        header("Location: ".ROOT."/php/index.php");
+    }
+    require dirname(__DIR__).'/php/phpPages/base.php';
+?>
