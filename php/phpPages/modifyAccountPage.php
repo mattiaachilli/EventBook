45a0ac3e82f4a273a65b348b58bd6daede7ab78f
Christian D'Errico <christian.derrico@studio.unibo.it>
2019-12-09 12:34:33 +0100

    Now it's possible to remove accounts from modal

--- php/phpPages/modifyAccountPage.php
+++ php/phpPages/modifyAccountPage.php
@@ -35,13 +35,13 @@
                     <div class="col-12 col-md-8 p-2">
                         <button type="submit" id = "salvaButton" class="btn btn-primary mb-2 float-right">Salva</button>
                         <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">Elimina</button>
+                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalDel">Elimina</button>
                         <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal fade" id="modalDel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <h5 class="modal-title" id="exampleModalLabel">Elimina account</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                                 </button>
