4ea1954d1dd915b9fc0cd8dfbc8fbce096418b48
Matteo Innocenti <matteo.innocenti2@studio.unibo.it>
2019-12-11 20:30:00 +0100

    Home is almost empty

--- php/home.php
+++ php/home.php
@@ -1,9 +1,2 @@
-<font size="5" color="white">Questa home è solo per provare le varie pagine!!!</font>
-<br>
-<font size="13"><a href="changePassword.php">Cambia Password</a></font>
-<br>
-<font size="13"><a href="events.php">Eventi in programma</a></font>
-<br>
-<font size="13"><a href="sendMail.php">Invio mail</a></font>
-<br>
-<font size="13"><a href="adminApprovazione.php">Admin HomePage</a></font>
+<p class="text-center"><font size="13"><a href="events.php">Eventi in programma</a></font></p>
+<p class="text-center"><font size="13"><a href="adminApprovazione.php">Admin HomePage</a></font></p>
