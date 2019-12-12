39a8defd3304cde8dc9b0b56552173e59256f52a
Matteo Innocenti <matteo.innocenti2@studio.unibo.it>
2019-12-12 12:13:17 +0100

    Images deleted; events page is the new home page

--- php/index.php
+++ php/index.php
@@ -1,8 +1,20 @@
 <?php
     require_once(dirname(__DIR__)."/php/bootstrap.php");
 
-    $parameters["title"] = "Home - EventBook";
-    $parameters["content"] = dirname(__DIR__)."/php/home.php";
+    $currentID = 0;
+    $nextOrPrev = 0;
+    if (isset($_GET["maxID"])) {
+        $currentID = $_GET["maxID"];
+        $nextOrPrev = 0;
+    }
+    if (isset($_GET["minID"])) {
+        $currentID = $_GET["minID"];
+        $nextOrPrev = 1;
+    }
+    $parameters["title"] = "Eventi in programma - EventBook";
+    $parameters["content"] = dirname(__DIR__)."/php/phpPages/eventsPage.php";
+    $parameters["events"] = $db->getNextOrPrevEvents($currentID, $nextOrPrev);
+    $parameters["content"] = dirname(__DIR__)."/php/phpPages/eventsPage.php";
     
     require dirname(__DIR__).'/php/phpPages/base.php';
 ?>
